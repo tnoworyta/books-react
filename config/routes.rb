edff4b090de3fe20ac23d5d5acf9ec6bcf123dda
tnoworyta <tomasz.noworyta@gmail.com>
2017-01-09 11:34:05 +0100

    React on Rails setup and example controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
     namespace :api do
       namespace :v1 do
         resources :books, only: [:index, :create, :destroy, :update]
