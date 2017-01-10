373fc8c50415ef8aa0abee8ccd457766fd8707bd
tnoworyta <tomasz.noworyta@gmail.com>
2017-01-10 11:42:11 +0100

    WIP

--- client/app/bundles/HelloWorld/startup/HelloWorldApp.jsx
+++ client/app/bundles/HelloWorld/startup/HelloWorldApp.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import configureStore from '../store/helloWorldStore';
+import { createStore } from 'redux'
+import todoApp from '../reducers'
 import App from '../components/App'
 
 // See documentation for https://github.com/reactjs/react-redux.
