f6b76a2f3c2865bacdf5c6f6f7e90d80e6889ccd
tnoworyta <tomasz.noworyta@gmail.com>
2017-01-10 10:07:34 +0100

    Remove non-redux files

--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -1,11 +1,9 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../components/HelloWorld';
-import BookList from '../components/BookList';
 
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorld,
-  BookList
 });
